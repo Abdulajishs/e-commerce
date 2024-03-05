0a1d9e9011c8ef3a92c48de7223e39ecf078badc
Abdulajishs <abdulajeeshsb@gmail.com>
2024-03-05 13:15:08 +0530

    Protecting the product page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Store from './pages/Store';
@@ -7,19 +7,24 @@ import RootLayout from './components/Layout/RootLayout/Root';
 import Contact from './pages/Contact';
 import ProductDetails from './pages/ProductDetails';
 import Login from './pages/Login';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
 
 
 function App() {
+  const authCntx = useContext(AuthContext)
   return (
     <>
       <RootLayout >
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path="store" element={<Store />} />
+          {authCntx.isLoggedIn && <Route path="store" element={<Store />} /> }
+          {!authCntx.isLoggedIn && <Route path="store" element={<Login />} /> }
           <Route path="about" element={<About />} />
           <Route path="login" element={<Login />} />
           <Route path="contact" element={<Contact />} />
           <Route path='product/:id' element={<ProductDetails />} />
+          <Route path='*' element = {<Navigate to="login"/>} />
         </Routes>
       </RootLayout>
     </>
